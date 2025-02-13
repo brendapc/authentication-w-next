92e9297d7dda05c3a3d24fdac39e5669b2077633
Brenda Pereira <brendacamara@outlook.com>
2025-02-13 15:59:16 -0300

    feat: use query param to adapt form to login/signup

--- components/auth-form.js
+++ components/auth-form.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { signup } from '@/actions/auth-actions';
 import { useFormState } from 'react-dom';
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
   const [formState, formAction] = useFormState(signup, {})
   return (
     <form id="auth-form" action={formAction}>
@@ -27,11 +27,12 @@ export default function AuthForm() {
       }
       <p>
         <button type="submit">
-          Create Account
+          {mode === 'login' ? 'Login' : 'Create Account'}
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === 'login' && <Link href="/?mode=signup">Create an account.</Link> }
+        {mode === 'signup' && <Link href="/?mode=login">Login with existing account.</Link> }
       </p>
     </form>
   );
